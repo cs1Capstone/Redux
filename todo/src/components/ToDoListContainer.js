679d4b6f527e051b8969258791783fc8eb5f2539
Tyge <tygesjohnson@gmail.com>
2018-02-21 14:47:25 -0700

    todolistcontainer wip

---
+++ todo/src/components/ToDoListContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addTodo, toggleTodo, removeTodos, getTodos } from '../actions';
+
+class TodoList extends Component {
+  constructor() {
+    super();
+    this.state ={
+      text: ''
+    };
+  };
+
+  
+}
